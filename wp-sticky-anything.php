2e37a3cb324ed2e1d795b8980deef13da3d71af8
monzuralam <monzur707@gmail.com>
2021-09-27 18:00:59 +0600

    rename file

---
+++ wp-sticky-anything.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name:       WP Sticky Anything
+ * Plugin URI:        https://wordpress.org/plugins/wp-sticky-anything
+ * Description:       WP Sticky Anything plugin will make stick to the side of page after when scrolled up & down.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.0
+ * Author:            Monzur Alam
+ * Author URI:        https://profiles.wordpress.org/monzuralam
+ * Text Domain:       wp-sticky-anything
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and 
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define('wp-sticky-anything', '1.0.0');
